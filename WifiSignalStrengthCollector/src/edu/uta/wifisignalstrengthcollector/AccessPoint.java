090088944e015b8fb3d2ea6218cd4fc5cd80c371
aduyng <aduyng@gmail.com>
2011-11-20 18:24:34 +0000

    initial import

---
+++ WifiSignalStrengthCollector/src/edu/uta/wifisignalstrengthcollector/AccessPoint.java
@@ -0,0 +1,44 @@
+package edu.uta.wifisignalstrengthcollector;
+
+import java.util.Date;
+
+
+public class AccessPoint {
+	String macAddress; 
+	int signalLevel; 
+	int distance;
+	Date timestamp; 
+	int id; 
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getMacAddress() {
+		return macAddress;
+	}
+	public void setMacAddress(String macAddress) {
+		this.macAddress = macAddress.toUpperCase();
+	}
+	public int getSignalLevel() {
+		return signalLevel;
+	}
+	public void setSignalLevel(int signalLevel) {
+		this.signalLevel = signalLevel;
+	}
+	public int getDistance() {
+		return distance;
+	}
+	public void setDistance(int distance) {
+		this.distance = distance;
+	} 
+	
+}
